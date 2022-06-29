067b64be784854fb84e1589ec2bad3d4d4b2b106
Cyril Gouv <cyril@macbook-air-de-samira.home>
2022-06-29 10:11:52 +0200

    🔨 - add controllers && services for our custom routes && sanitize output data

--- src/api/classroom/controllers/classroom.js
+++ src/api/classroom/controllers/classroom.js
@@ -5,11 +5,18 @@
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
+const { sanitize } = require('@strapi/utils')
 
 module.exports = createCoreController('api::classroom.classroom', ({ strapi }) => ({
     
-    async findTutorials() {
-        return 'To be implemted'
+    async findTutorials(ctx) {
+        const { params } = ctx
+        const results = await strapi.service('api::classroom.classroom').findTutorials(params.id)
+        
+        const model = strapi.getModel('api::tutorial.tutorial')
+        const sanitizedResults = await sanitize.contentAPI.output(results, model)
+        
+        return this.transformResponse(sanitizedResults)
     }
 
 }));
--- src/api/classroom/services/classroom.js
+++ src/api/classroom/services/classroom.js
@@ -6,4 +6,12 @@
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::classroom.classroom');
+module.exports = createCoreService('api::classroom.classroom', ({ strapi }) => ({
+    findTutorials(classroomId) {
+        return strapi.entityService.findMany('api::tutorial.tutorial', {
+            filters: { classroom: classroomId },
+            populate: ['classroom', 'classroom.manager', 'coverImage'],
+        })
+    },
+    
+}))
