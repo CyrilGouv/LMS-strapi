c4c2531192938ac9fc8c5804a372d285293c5b47
Cyril Gouv <cyril@macbook-air-de-samira.home>
2022-07-04 15:18:45 +0200

    handle tutorials policy manager

--- src/api/tutorial/policies/is-manager.js
+++ src/api/tutorial/policies/is-manager.js
@@ -4,15 +4,28 @@
  * `is-manager` policy.
  */
 
-module.exports = (policyContext, config, { strapi }) => {
-    // Add your own logic here.
-    strapi.log.info('In is-manager policy.');
+module.exports = async (policyContext, config, { strapi }) => {
+    
+  try {
+    // Get the id from req
+    const { id } = policyContext.params
 
-    const canDoSomething = true;
+    // Get the tutorial thanks to req id
+    const tutorial = await strapi.service('api::tutorial.tutorial').findOne(id, { populate: ['classroom.manager'] })
 
-    if (canDoSomething) {
-      return true;
-    }
+    // Current logged in user
+    const { user } = policyContext.state
 
-    return false;
+    // Check if classroom manager is logged in user
+    const { classroom: { manager } } = tutorial
+
+    if (manager && manager.id === user.id) return true
+
+    return false
+
+  } catch (e) {
+    strapi.log.error('Error in is-manager policy', e)
+    return false
+  }
+
 };
